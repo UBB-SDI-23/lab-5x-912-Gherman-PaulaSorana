bb222de7da89ee079a4875918c6aef808c93387f
Gherman Sorana <ghermansorana@yahoo.com>
2023-03-31 22:11:13 +0300

    material UI first try

--- frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
+++ frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
@@ -1,3 +1,18 @@
+import {
+	TableContainer,
+	Paper,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	CircularProgress,
+	Container,
+	IconButton,
+	Tooltip,
+} from "@mui/material";
+
+
 import { useEffect, useState } from "react";
 import { GlobalURL } from "../../main";
 import { Swimmer } from "../../models/Swimmer";
@@ -20,33 +35,39 @@ export const SwimmerShowAll = () => {
     }
     else
         return (
-        <div className="App">
-            <h1>Swimmers List</h1>
-            <table>
-                <tr>
-                    <th>#</th>
-                    <th>Last Name</th>
-                    <th>First Name</th>
-                    <th>County</th>
-                    <th>Date of Birth</th>
-                    <th>Years of experience</th>
-                    <th>Team ID</th>
-                    <th>Fans IDS</th>
-                </tr>
-                {swimmers.map((swimmer:Swimmer, index) => (
-                    <tr key={index}>
-                        <td>{index}</td>
-                        <td>{swimmer.swimmer_last_name}</td>
-                        <td>{swimmer.swimmer_first_name}</td>
-                        <td>{swimmer.swimmer_county}</td>
-                        <td>{swimmer.swimmer_date_of_birth}</td>
-                        <td>{swimmer.swimmer_years_of_experience}</td>
-                        <td>{swimmer.team}</td>
-                        <td>{swimmer.fans}</td>
-                    </tr>
-                ))}
-            </table>
-        </div>
+        <Container>
+			<h1>All courses</h1>
+
+                <TableContainer component={Paper}>
+					<Table sx={{ minWidth: 650 }} aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell>#</TableCell>
+								<TableCell align="right">Last Name</TableCell>
+								<TableCell align="right">First Name</TableCell>
+								<TableCell align="right">County</TableCell>
+								<TableCell align="center">Date Of Birth</TableCell>
+                                <TableCell align="center">Years of experience</TableCell>
+                                <TableCell align="center">Team ID</TableCell>
+                                <TableCell align="center">Fans IDS</TableCell>
+                            </TableRow>
+						</TableHead>
+                        <TableBody>
+							{swimmers.map((swimmer:Swimmer, index) => (
+                                <TableRow key={index}>
+                                    <TableCell align="right">{swimmer.swimmer_last_name}</TableCell>
+                                    <TableCell align="right">{swimmer.swimmer_first_name}</TableCell>
+                                    <TableCell align="right">{swimmer.swimmer_county}</TableCell>
+                                    <TableCell align="right">{swimmer.swimmer_date_of_birth}</TableCell>
+                                    <TableCell align="right">{swimmer.swimmer_years_of_experience}</TableCell>
+                                    <TableCell align="right">{swimmer.team}</TableCell>
+                                    <TableCell align="right">{swimmer.fans}</TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+					</Table>
+				</TableContainer>
+        </Container>
         );
-  }
+  };
   
