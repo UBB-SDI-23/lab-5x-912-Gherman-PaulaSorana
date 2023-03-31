1cc39a66e07c664f91d3ac40e1954f690815e91a
Gherman Sorana <ghermansorana@yahoo.com>
2023-03-31 14:51:21 +0300

    https for netlify

--- frontend/react-frontend-a_5/src/main.tsx
+++ frontend/react-frontend-a_5/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const GlobalURL="http://13.50.226.167/";
+export const GlobalURL="https://13.50.226.167/";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
