3c474418c42aead3813c917665bf34964a304de6
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-01 16:10:38 +0300

    Update App.tsx

--- frontend/react-frontend-a_5/src/App.tsx
+++ frontend/react-frontend-a_5/src/App.tsx
@@ -22,7 +22,8 @@ function App() {
                       <Route path="/" element={<AppHome />} />
                       <Route path="/swimmers/add" element={<SwimmerAdd />} />
                       <Route path="/swimmers" element={<SwimmerShowAll />} />
-                      <Route path="/swimmers/:swimmerId/details" element={<SwimmerDetails />} />
+                      <Route path="/swimmers/:swimmerId" element={<SwimmerDetails />} />
+                      
               </Routes>
       </Router>
     </React.Fragment>
