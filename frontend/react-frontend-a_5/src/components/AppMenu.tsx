b8475bde8e6ddc9362e873e5bb881c8d56c3b41c
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-01 21:52:01 +0300

    nav

--- frontend/react-frontend-a_5/src/components/AppMenu.tsx
+++ frontend/react-frontend-a_5/src/components/AppMenu.tsx
@@ -10,7 +10,7 @@ export const AppMenu = () => {
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static" style={{top:0, width:100}}>
+			<AppBar position="static" style={{width:100}}>
 				<Toolbar variant="dense">
 					<IconButton
 						component={Link}
