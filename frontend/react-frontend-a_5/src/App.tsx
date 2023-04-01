c57e773b8a271acc013917d490c95a68d9093191
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-01 15:06:49 +0300

    apphome

--- frontend/react-frontend-a_5/src/App.tsx
+++ frontend/react-frontend-a_5/src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SwimmerDetails } from "./components/swimmers/SwimmerDetails";
 import { AppMenu } from "./components/AppMenu";
 import { SwimmerAdd } from "./components/swimmers/SwimmerAdd";
+import { AppHome } from "./components/AppHome";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,11 +19,11 @@ function App() {
               <AppMenu />
 
               <Routes>
-
+                      <Route path="/" element={<AppHome />} />
                       <Route path="/swimmers" element={<SwimmerShowAll />} />
                       <Route path="/swimmers/:swimmerId/details" element={<SwimmerDetails />} />
                       <Route path="/swimmers/add" element={<SwimmerAdd />} />
-                      
+
               </Routes>
       </Router>
     </React.Fragment>
