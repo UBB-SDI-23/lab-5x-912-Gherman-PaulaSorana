c5b5e401799b39335d62579b0382e31e9e7289d2
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-01 16:51:36 +0300

    delete

---
+++ frontend/react-frontend-a_5/src/components/SwimmerDelete.tsx
@@ -0,0 +1,39 @@
+import { Container, Card, CardContent, IconButton, CardActions, Button } from "@mui/material";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import axios from "axios";
+
+export const CourseDelete = () => {
+	const { courseId } = useParams();
+	const navigate = useNavigate();
+
+	const handleDelete = async (event: { preventDefault: () => void }) => {
+		event.preventDefault();
+		await axios.delete(`../api/swimmer/${courseId}/`);
+		
+		navigate("/swimmers");
+	};
+
+	const handleCancel = (event: { preventDefault: () => void }) => {
+		event.preventDefault();
+	
+		navigate("/swimmers");
+	};
+
+	return (
+		<Container>
+			<Card>
+				<CardContent>
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/swimmers`}>
+						<ArrowBackIcon />
+					</IconButton>{" "}
+					Are you sure you want to delete this swimmer? This cannot be undone!
+				</CardContent>
+				<CardActions>
+					<Button onClick={handleDelete}>Delete it</Button>
+					<Button onClick={handleCancel}>Cancel</Button>
+				</CardActions>
+			</Card>
+		</Container>
+	);
+};
