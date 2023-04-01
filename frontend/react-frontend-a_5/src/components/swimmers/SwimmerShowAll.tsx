eddeebc0ce8686a3ce1d2d53df55721e406195f3
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-01 12:14:26 +0300

    delete else

--- frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
+++ frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
@@ -34,48 +34,48 @@ export const SwimmerShowAll = () => {
     if (swimmers.length === 0){
         return <div>No swimmers</div>
     }
-    else
-        return (
-        <Container>
-			<h1>All Swimmers</h1>
+    
+    return (
+    <Container>
+        <h1>All Swimmers</h1>
 
-                <TableContainer component={Paper}>
-					<Table sx={{ minWidth: 800 }} aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>#</TableCell>
-								<TableCell align="center">Last Name</TableCell>
-								<TableCell align="center">First Name</TableCell>
-								<TableCell align="center">County</TableCell>
-								<TableCell align="center">Date Of Birth</TableCell>
-                                <TableCell align="center">Years of experience</TableCell>
-                                <TableCell align="center">Team ID</TableCell>
-                                <TableCell align="center">Fans IDS</TableCell>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 800 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>#</TableCell>
+                            <TableCell align="center">Last Name</TableCell>
+                            <TableCell align="center">First Name</TableCell>
+                            <TableCell align="center">County</TableCell>
+                            <TableCell align="center">Date Of Birth</TableCell>
+                            <TableCell align="center">Years of experience</TableCell>
+                            <TableCell align="center">Team ID</TableCell>
+                            <TableCell align="center">Fans IDS</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {swimmers.map((swimmer:Swimmer, index) => (
+                            <TableRow key={swimmer.id}>
+                                <TableCell component="th" scope="row">
+                                    {index + 1}
+                                <TableCell component="th" scope="row">
+                                    <Link to={`/swimmers/${swimmer.id}/details`} title="View swimmer details">
+                                        {"view details"}
+                                    </Link>
+                                </TableCell>
+                                </TableCell>
+                                <TableCell align="center">{swimmer.swimmer_last_name}</TableCell>
+                                <TableCell align="center">{swimmer.swimmer_first_name}</TableCell>
+                                <TableCell align="center">{swimmer.swimmer_county}</TableCell>
+                                <TableCell align="center">{swimmer.swimmer_date_of_birth}</TableCell>
+                                <TableCell align="center">{swimmer.swimmer_years_of_experience}</TableCell>
+                                <TableCell align="center">{swimmer.team}</TableCell>
+                                <TableCell align="center">{swimmer.fans}</TableCell>
                             </TableRow>
-						</TableHead>
-                        <TableBody>
-							{swimmers.map((swimmer:Swimmer, index) => (
-                                <TableRow key={swimmer.id}>
-                                    <TableCell component="th" scope="row">
-										{index + 1}
-                                    <TableCell component="th" scope="row">
-                                        <Link to={`/swimmers/${swimmer.id}/details`} title="View swimmer details">
-                                            {"view details"}
-                                        </Link>
-									</TableCell>
-									</TableCell>
-                                    <TableCell align="center">{swimmer.swimmer_last_name}</TableCell>
-                                    <TableCell align="center">{swimmer.swimmer_first_name}</TableCell>
-                                    <TableCell align="center">{swimmer.swimmer_county}</TableCell>
-                                    <TableCell align="center">{swimmer.swimmer_date_of_birth}</TableCell>
-                                    <TableCell align="center">{swimmer.swimmer_years_of_experience}</TableCell>
-                                    <TableCell align="center">{swimmer.team}</TableCell>
-                                    <TableCell align="center">{swimmer.fans}</TableCell>
-                                </TableRow>
-                            ))}
-                    </TableBody>
-					</Table>
-				</TableContainer>
-        </Container>
-        );       
+                        ))}
+                </TableBody>
+                </Table>
+            </TableContainer>
+    </Container>
+    );       
 };
