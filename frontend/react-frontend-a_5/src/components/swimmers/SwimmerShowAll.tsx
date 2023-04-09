ebe3f08fa4bac824ed22f4e8632b4580e984f366
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-09 20:27:05 +0300

    Update SwimmerShowAll.tsx

--- frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
+++ frontend/react-frontend-a_5/src/components/swimmers/SwimmerShowAll.tsx
@@ -99,6 +99,7 @@ export const SwimmerShowAll = () => {
                                     <Link to={`/swimmers/${swimmer.id}`} title="View swimmer details">
                                         {"view details"}
                                     </Link>
+                                    
                                 </TableCell>
                                 </TableCell>
                                 <TableCell align="center">{swimmer.swimmer_last_name}</TableCell>
