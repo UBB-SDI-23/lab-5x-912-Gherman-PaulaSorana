8cc10e58470665df6240cb511cc322b1ec8f0e2e
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-20 12:29:32 +0300

    .

--- backend/app1/Views/Pagination.py
+++ backend/app1/Views/Pagination.py
@@ -1,7 +1,30 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import CursorPagination
 
 
-class CustomPagination(PageNumberPagination):
+class CustomPagination(CursorPagination):
     page_size = 10
     max_page_size = 100
-    page_size_query_param = "page_size"
+    ordering = 'id'
+
+    # Override `get_cursor()` to return the cursor value for the next page of results
+    def get_cursor(self, queryset):
+        if len(self.page) == 0:
+            return None
+        return getattr(self.page[-1], self.ordering)
+
+    # Override `paginate_queryset()` to return the paginated results
+    def paginate_queryset(self, queryset, request, view=None):
+        # Get the cursor value from the request
+        cursor = request.query_params.get('cursor')
+
+        # Set the initial queryset and ordering
+        qs = queryset.order_by(self.ordering)
+
+        # If a cursor is provided, filter the queryset based on the cursor
+        if cursor:
+            qs = qs.filter(**{f"{self.ordering}__lt": cursor})
+
+        # Use Django's built-in pagination methods to paginate the queryset
+        page = self.django_paginator.get_page(qs, self.get_page_size(request), self.get_max_page_size(request))
+
+        return page
