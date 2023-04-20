71802dd52808b25b3939e7b545ce3e119ea46345
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-20 14:41:23 +0300

    Update models.py

--- backend/app1/models.py
+++ backend/app1/models.py
@@ -55,7 +55,7 @@ class Fan(models.Model):
 
     class Meta:
         ordering = ['id']
-        indexes = [models.Index(fields=["fan_first_name", "fan_last_name", "id"])]
+        indexes = [models.Index(fields=["id"])]
 
 
 class SwimmerFan(models.Model):
