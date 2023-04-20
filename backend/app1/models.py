239821c78a374992a322ad95739ab444d2fa1de2
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-20 16:48:01 +0300

    .

--- backend/app1/models.py
+++ backend/app1/models.py
@@ -55,7 +55,7 @@ class Fan(models.Model):
 
     class Meta:
         ordering = ['id']
-        # indexes = [models.Index(fields=["id"])]
+        indexes = [models.Index(fields=["fan_email"])]
 
 
 class SwimmerFan(models.Model):
