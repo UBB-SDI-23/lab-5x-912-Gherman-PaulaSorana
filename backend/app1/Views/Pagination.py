28be685d6131697af6ee0dc67e14ff2707c4a629
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-20 12:49:22 +0300

    .

--- backend/app1/Views/Pagination.py
+++ backend/app1/Views/Pagination.py
@@ -1,5 +1,4 @@
 from rest_framework.pagination import PageNumberPagination
-from django.core.paginator import Paginator
 
 
 class CustomPagination(PageNumberPagination):
@@ -7,11 +6,4 @@ class CustomPagination(PageNumberPagination):
     max_page_size = 100
     page_size_query_param = 'page_size'
 
-    # Override `paginate_queryset()` to return the paginated results
-    def paginate_queryset(self, queryset, request, view=None):
-        # Use Django's built-in Paginator class to paginate the queryset
-        paginator = Paginator(queryset, self.get_page_size(request))
-        page_number = request.query_params.get(self.page_query_param, 1)
-        page = paginator.get_page(page_number)
 
-        return page
