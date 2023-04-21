f2bdf1d541c3bd668adea2be9d2b5548129bee1a
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-21 15:30:40 +0300

    .

--- frontend/react-frontend-a_5/src/components/Statistics.tsx
+++ frontend/react-frontend-a_5/src/components/Statistics.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Statistics = () => {
   return (
     <Container>
-      <Button style={{color:"whitesmoke", border:"white"}} component={Link} sx={{ mr: 3 }} to="/statisticsteam/teamsOrd">
+      <Button style={{color:"whitesmoke", border: '1px solid whitesmoke'}} component={Link} sx={{ mr: 3 }} to="/statisticsteam/teamsOrd">
         Teams Ordered By No Of Swimmers
       </Button>
     </Container>
