d6d6db427d9fdef631183b85697836e454478a48
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-21 10:41:15 +0300

    .

--- frontend/react-frontend-a_5/src/components/swimmerfans/SwimmerFanDetails.tsx
+++ frontend/react-frontend-a_5/src/components/swimmerfans/SwimmerFanDetails.tsx
@@ -36,8 +36,8 @@ export const SwimmerFanDetails = () => {
 					<h1 style={{textAlign:"left", fontWeight:'bold'}}>SwimmerFan Details</h1>
 					<p  style={{textAlign:"left", fontWeight:'bold'}}>Fan Page Name: {swimmer?.fan_page_name}</p>
 					<p  style={{textAlign:"left", fontWeight:'bold'}}>Fan Since Year: {swimmer?.fan_since_year}</p>
-                    <p  style={{textAlign:"left", fontWeight:'bold'}}>Swimmers: {swimmer?.swimmer}</p>
-                    <p  style={{textAlign:"left", fontWeight:'bold'}}>Fans: {swimmer?.fan}</p>
+                    <p  style={{textAlign:"left", fontWeight:'bold'}}>Swimmer: {swimmer?.swimmer}</p>
+                    <p  style={{textAlign:"left", fontWeight:'bold'}}>Fan: {swimmer?.fan}</p>
 				</CardContent>
 
 
