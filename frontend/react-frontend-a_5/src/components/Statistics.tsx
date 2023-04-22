6ae8d4c2d19b95ddc8fd9a9f3f99d1ce905500ec
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-22 13:21:06 +0300

    inline buttons

--- frontend/react-frontend-a_5/src/components/Statistics.tsx
+++ frontend/react-frontend-a_5/src/components/Statistics.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Statistics = () => {
   return (
-    <Container>
+    <Container sx={{ display: 'flex', flexDirection: 'row' }}>
       <Button style={{color:"whitesmoke", border: '1px solid whitesmoke'}} component={Link} sx={{ mr: 3 }} to="/statisticsteam/teamsOrd">
         Teams Ordered By No Of Swimmers
       </Button>
