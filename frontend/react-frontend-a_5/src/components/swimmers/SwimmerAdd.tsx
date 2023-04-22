16ec5add4dd6468c46e46ecc8c0b626a62178c38
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-22 15:27:54 +0300

    /

--- frontend/react-frontend-a_5/src/components/swimmers/SwimmerAdd.tsx
+++ frontend/react-frontend-a_5/src/components/swimmers/SwimmerAdd.tsx
@@ -32,6 +32,7 @@ const navigate = useNavigate();
 			);
 			const data = await response.data;
 			setTeams(data);
+			console.log(data);
 		} catch (error) {
 			console.error("Error fetching suggestions:", error);
 		}
