1df2b4de6ca56957e1e485090b9f8caa64e79cc0
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-23 17:16:03 +0300

    .

--- frontend/react-frontend-a_5/src/components/fans/FanAdd.tsx
+++ frontend/react-frontend-a_5/src/components/fans/FanAdd.tsx
@@ -24,7 +24,7 @@ const navigate = useNavigate();
 		try {
 			const response = await axios.post(`${BACKEND_API_URL}/fan/`, fan);
 			
-			if (response.status < 200 || response.status >= 300) {
+			if (response.status < 200 || response.status >= 300 || response.status===400) {
 				throw new Error("This email is already in use!");
 			}
 			else{
