7b4177787830de68f25341b2a14024579e2c2d41
Gherman Sorana <ghermansorana@yahoo.com>
2023-04-24 11:43:48 +0300

    .

--- frontend/react-frontend-a_5/src/components/pagination/Pagination.tsx
+++ frontend/react-frontend-a_5/src/components/pagination/Pagination.tsx
@@ -1,57 +1,130 @@
+import { Button } from "@mui/material";
 import { debounce } from "lodash";
-import { useCallback, useEffect } from "react";
+import React from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
-export {};
-
-interface PaginatorProps {
-    rowsPerPage: number;
-    totalRows: number;
-    currentPage: number;
-    isFirstPage: boolean;
-    isLastPage: boolean;
-    setPage: (page: number) => void;
-    goToNextPage: () => void;
-    goToPrevPage: () => void;
+export interface PaginatorProps {
+  rowsPerPage: number;
+  totalRows: number;
+  currentPage: number;
+  isFirstPage: boolean;
+  isLastPage: boolean;
+  setPage: (page: number) => void;
+  goToNextPage: () => void;
+  goToPrevPage: () => void;
 }
 
-export const Paginator = ({ rowsPerPage, totalRows, currentPage, isFirstPage, isLastPage, setPage, goToNextPage, goToPrevPage }: PaginatorProps) => {
+export const Paginator = ({
+  rowsPerPage,
+  totalRows,
+  currentPage,
+  isFirstPage,
+  isLastPage,
+  setPage,
+  goToNextPage,
+  goToPrevPage,
+}: PaginatorProps) => {
+  const totalPages = Math.ceil(totalRows / rowsPerPage);
+  const pageRange = 11;
 
-    const totalPages = Math.ceil(totalRows / rowsPerPage);
-
-    const changeCurrentPage = (pageNumber: number) => {
-        if (pageNumber < 1) {
-            setPage(1);
-            return;
-        }
-
-        else if (pageNumber > totalPages) {
-            setPage(totalPages);
-            return;
-        }
-
-        setPage(pageNumber);
+  const changeCurrentPage = (pageNumber: number) => {
+    if (pageNumber < 1) {
+      setPage(1);
+      
+      return;
+    } else if (pageNumber > totalPages) {
+      setPage(totalPages);
+      return;
     }
 
-    const debounceOnChange = useCallback(debounce(changeCurrentPage, 500), []);
+    setPage(pageNumber);
+  };
 
-    useEffect(() => {
-        return () => {
-            debounceOnChange.cancel();
-        };
-    }, [debounceOnChange])
+  const debounceOnChange = useCallback(debounce(changeCurrentPage, 500), []);
 
-    return (
-        <div className='pagination'>
+  useEffect(() => {
+    return () => {
+      debounceOnChange.cancel();
+    };
+  }, [debounceOnChange]);
 
-            <button className='floating' disabled={isFirstPage} onClick={() => setPage(1)}>First</button>
-            <button className='floating' disabled={isFirstPage} onClick={() => goToPrevPage()}>Prev</button>
+  const visiblePages = useMemo(() => {
+    const pageNumbers = [];
 
-            <span className='floating'>
-                Page <input onChange={(event) => debounceOnChange(Number(event.target.value))} type="number" className='page-input' defaultValue={currentPage} /> of <strong>{totalPages}</strong>
-            </span>
+    debugger;
+    if (totalPages <= pageRange) {
+      for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+      }
+    } else {
+      let start = Math.max(currentPage - Math.floor(pageRange / 2), 1);
+      let end = Math.min(start + pageRange - 1, totalPages);
 
-            <button className='floating' disabled={isLastPage} onClick={() => goToNextPage()}>Next</button>
-            <button className='floating' disabled={isLastPage} onClick={() => setPage(totalPages)}>Last</button>
-        </div >
-    )
-}
+      if (end - start < pageRange - 1) {
+        start = Math.max(end - pageRange + 1, 1);
+      }
+
+      for (let i = start; i <= end; i++) {
+        pageNumbers.push(i);
+      }
+
+      if (start - 5 > 1 && start - 5 >= 1) {
+        pageNumbers.unshift("...");
+      }
+      for (let i = Math.min(start-1, 5); i >= 1; --i) {
+        pageNumbers.unshift(i);
+      }
+
+
+      if (end < totalPages-4) {
+        pageNumbers.push("...");
+        // pageNumbers.push(totalPages-4);
+        // pageNumbers.push(totalPages-3);
+        // pageNumbers.push(totalPages-2);
+        // pageNumbers.push(totalPages-1);
+        // pageNumbers.push(totalPages);
+      }
+
+      for (let i = totalPages-4; i <= Math.max(end, totalPages); ++i) {
+        pageNumbers.push(i);
+      }
+
+    }
+
+    return pageNumbers;
+  }, [totalPages, currentPage]);
+
+  return (
+    <div className="pagination">
+      {/* <button
+        className="floating"
+        disabled={isFirstPage}
+        onClick={() => setPage(1)}
+      >
+        1
+      </button> */}
+
+      {visiblePages.map((page, index) => (
+        <React.Fragment key={index}>
+          
+            <Button
+              variant={page === currentPage ? "contained" : "outlined"}
+              disabled={page==="..."}
+              onClick={() => setPage(Number(page))}
+            >
+              {page}
+            </Button>
+          
+        </React.Fragment>
+      ))}
+
+      {/* <button
+        className="floating"
+        disabled={isLastPage}
+        onClick={() => setPage(totalPages)}
+      >
+        {totalPages}
+      </button> */}
+    </div>
+  );
+};
